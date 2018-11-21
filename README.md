6502171cb38276e8df4ab3734ec1d1175e3de6e1
Pipher Chen <1095658929@qq.com>
2018-11-21 10:32:16 +0800

    添加maven多环境

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 #es相关
 sh /home/es/elasticsearch-5.5.2/bin/elasticsearch -d
+
+
+http://localhost:8001/api-elasticsearch-demo/dev/master
--- api-elasticsearch-demo/src/test/java/com/example/demo/es/ApiElasticsearchDemoApplicationTests.java
+++ api-elasticsearch-demo/src/test/java/com/example/demo/es/ApiElasticsearchDemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.demo.es;
 
+import com.example.demo.es.dao.ItemRepo;
+import com.example.demo.es.model.Item;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +24,9 @@ public class ApiElasticsearchDemoApplicationTests {
 	@Value("${spring.data.elasticsearch.repositories.enabled}")
 	private static String enabled;
 
+	@Autowired
+	private ItemRepo itemRepo;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -28,6 +36,17 @@ public class ApiElasticsearchDemoApplicationTests {
 		System.out.println(clusterName);
 		System.out.println(clusterNodes);
 		System.out.println(enabled);
+	}
+
+	@Test
+	public void testQuery() {
+		// 词条查询
+//        MatchQueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "小米").operator(Operator.AND);
+		WildcardQueryBuilder fileType = QueryBuilders.wildcardQuery("brand", "*小*");
+		// 执行查询
+//        Iterable<Item> items = this.repository.search(queryBuilder);
+		Iterable<Item> items = this.itemRepo.search(fileType);
+		items.forEach(System.out::println);
 	}
 
 }
